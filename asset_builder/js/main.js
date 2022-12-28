39ba5e1c92cc9fd842b96c63eabe4a3ca154f8b1
RubyRubz <obongrubie260@gmail.com>
2022-12-29 00:57:50 +0800

    added buttons for mode selection

--- asset_builder/js/main.js
+++ asset_builder/js/main.js
@@ -7,10 +7,9 @@ const animate = require('./includes/animations.js');
 const detect  = require('./includes/detect.js');
 
 const calculator = {
-  n1: '0', //initial concatenated digits
-  n2: '0', //last concatenated digits
-  operator: null, //mathematical operations that should be initiated
-  equals: false, // if true... it will execute ===> n1 (operator) n2  
+  numberArray: [],
+  numberPlaceholder: ['0', null],
+  equals: false, // if true... it will do computations
   mode: 'Simple', //default mode of calculator i.e.
   modes: ['Simple', 'Scientific', 'Converter'], //array of calculator modes (i.e. everytime mode button is clicked it will cycle to the next mode of this array)      
 }
