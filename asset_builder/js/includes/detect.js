5dc7d5bb29b526f129afd88f28f570914e2ee9a0
RubyRubz <obongrubie260@gmail.com>
2023-01-01 16:44:24 +0800

    isolated functions button code to new file

--- asset_builder/js/includes/detect.js
+++ asset_builder/js/includes/detect.js
@@ -2,6 +2,7 @@
 const simple = require('./simple.js');
 const scientific  = require('./scientific.js');
 const convert  = require('./convert.js');
+const functions  = require('./functions.js');
 
 
 exports.click =  (e, calculator) => { //anonymous function in es6 format
@@ -135,37 +136,7 @@ exports.click =  (e, calculator) => { //anonymous function in es6 format
     //insert [number, operator] to numberArrays
     calculator.numberArray.push([calculator.numberPlaceholder[0], e.target.dataset.value])
     
-      switch(e.target.dataset.value) {
-        case 'reciprocal':
-        
-        calculator.numberPlaceholder = 1/(calculator.numberPlaceholder[0]);
-        screenNumber = calculator.numberPlaceholder
-        console.log(screenNumber)
-        calculator.numberArray.pop([calculator.numberPlaceholder[0], e.target.dataset.value])
-        
-        break;
-        
-        case 'square':
-        calculator.numberPlaceholder = calculator.numberPlaceholder[0] ** 2;
-        
-        screenNumber = calculator.numberPlaceholder
-        console.log(screenNumber)
-        
-        calculator.numberArray.pop([calculator.numberPlaceholder[0], e.target.dataset.value])
-        
-        break;
-        case 'squareRoot':
-        calculator.numberPlaceholder = Math.sqrt(calculator.numberPlaceholder[0])
-        screenNumber = calculator.numberPlaceholder
-        console.log(screenNumber)
-        
-        calculator.numberArray.pop([calculator.numberPlaceholder[0], e.target.dataset.value])
-        
-        break;
-        
-        default:
-        // code block     
-      }  
+    functions.buttons(e, calculator, screenNumber);
     
     //reset placeholder
     calculator.numberPlaceholder = [screenNumber, null];
