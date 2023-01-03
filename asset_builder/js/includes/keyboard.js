9cb4626b92c690067f63d95c5ea26c7aed0ce572
RubyRubz <obongrubie260@gmail.com>
2023-01-03 18:59:05 +0800

    added keyboard listener

--- asset_builder/js/includes/keyboard.js
+++ asset_builder/js/includes/keyboard.js
@@ -1,7 +1,4 @@
 exports.type = (event, value) => { //anonymous function in es6 format
-  //alert(`I should click the button ${value}`)
-  console.log(event);
-
+  //this will find element in index.html that has an id="num_XXX" base on the event.key
   document.getElementById("num_" + event.key).click();
-
 };
